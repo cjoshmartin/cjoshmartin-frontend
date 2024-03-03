8dee21010a3ed100d2606c24f2bf746dca8f0946
Josh Martin <joshmartin@reprogrammed.software>
2024-03-03 00:12:36 -0600

    Fix up footer

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -4,7 +4,11 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.innerFooter}>
-        <span>
+        <span
+        style={{
+          padding: '1rem'
+        }}
+        >
           Handmade by <a href="https://github.com/cjoshmartin" target='_blank'>@cjoshmartin</a>,
           with <a href="https://www.djangoproject.com/" target='_blank'>Django</a>, <a href='https://nextjs.org/'>Next.js</a> and{" "}
           <a href="https://wagtail.org/" target='_blank'>Wagtail</a> (©{new Date().getFullYear()})
