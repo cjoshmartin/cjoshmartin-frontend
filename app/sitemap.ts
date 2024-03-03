e1dfb3b5f997ba3fe701335e21bd99129efaac2b
Josh Martin <joshmartin@reprogrammed.software>
2024-03-02 21:06:46 -0600

    generate sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,44 @@
+import { MetadataRoute } from 'next'
+import { getPages } from './components/api/pages';
+import { PageTypes } from './PageTypes';
+
+export default async function sitemap() :Promise<MetadataRoute.Sitemap> {
+
+    const baseURL = "https:/cjoshmartin.com"
+    const pages = await getPages();
+
+    //@ts-ignore
+    return pages.map((page) => {
+        const {meta} = page;
+        let url = baseURL;
+        let changeFrequency = 'yearly';
+
+        switch(meta.type){
+            case PageTypes.HOME:
+                url += '/'
+                changeFrequency = 'weekly';
+                break;
+            case PageTypes.BLOG_POST:
+                url += `/blog/${meta.slug}`
+                break;
+            case PageTypes.PROJECT:
+                url += `/projects/${meta.slug}`
+                break;
+            default:
+                return undefined;
+        }
+        let lastModified = meta?.first_published_at;
+
+        if (page?.data) {
+            lastModified = new Date(page?.data).toISOString()
+        }
+
+
+        return {
+            url,
+            lastModified,
+            changeFrequency
+        }
+    })
+    .filter((data) => !!data)
+}
