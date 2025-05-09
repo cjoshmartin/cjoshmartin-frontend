86e02a34edb8b2de085ed6e8cea6f7c9155298f6
Josh Martin <joshmartin@reprogrammed.software>
2025-05-09 13:47:36 -0500

    googl an -> Posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { GoogleAnalytics } from '@next/third-parties/google'
 import { IBM_Plex_Mono } from 'next/font/google'
 import './globals.css'
 import Nav from './components/layout/Nav';
 import Footer from './components/layout/Footer';
+import { PostHogProvider } from './components/PostHogProvider';
 
 import styles from './layout.module.css'
 
@@ -11,7 +11,6 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import AnimationContainer from './components/layout/AnimationContainer';
 import LoaderContainer from './components/Loader/LoaderContainer';
-import GoogleAdsense from './components/GoogleAdsense';
 config.autoAddCss = false;
 
 
@@ -35,22 +34,22 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div 
-        className={styles.bodyContent}
-        >
-          <div className={styles.innerBodyContent}>
-            <Nav />
-            <LoaderContainer>
-              <AnimationContainer>
-                {children}
-                <Footer />
-              </AnimationContainer>
-            </LoaderContainer>
+        <PostHogProvider>
+          <div 
+            className={styles.bodyContent}
+          >
+            <div className={styles.innerBodyContent}>
+              <Nav />
+              <LoaderContainer>
+                <AnimationContainer>
+                  {children}
+                  <Footer />
+                </AnimationContainer>
+              </LoaderContainer>
+            </div>
           </div>
-        </div>
+        </PostHogProvider>
       </body>
-      {/* <GoogleAdsense pId="8241145315698443"/> */}
-      {/* <GoogleAnalytics gaId="G-P8XH0GSQTV" /> */}
     </html>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,20 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        "images.unsplash.com",
-        "nyc3.digitaloceanspaces.com"
-      ],
-        remotePatterns: [
-          {
-            protocol: "http",
-            hostname: "45.55.48.61",
-            port: "",
-            pathname: "/media/**",
-          },
-        ],
-        dangerouslyAllowSVG: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "nyc3.digitaloceanspaces.com"
+    ],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "45.55.48.61",
+        port: "",
+        pathname: "/media/**",
       },
-}
+    ],
+    dangerouslyAllowSVG: true,
+  },
 
-module.exports = nextConfig
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/ingest/decide",
+        destination: "https://us.i.posthog.com/decide",
+      },
+    ];
+  },
+
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+};
+
+module.exports = nextConfig;
