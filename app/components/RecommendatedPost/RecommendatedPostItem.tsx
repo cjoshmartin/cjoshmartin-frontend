2387d57cfb5a5ea422849666ec592f51e63c20e0
Josh Martin <joshmartin@reprogrammed.software>
2025-08-29 12:29:18 -0500

    It looks good enough to ship

--- app/components/RecommendatedPost/RecommendatedPostItem.tsx
+++ app/components/RecommendatedPost/RecommendatedPostItem.tsx
@@ -4,7 +4,12 @@ import styles from "./RecommendatedPostItem.module.css";
 export function RecommendatedPostItem({post}: {post: any}) {
   return (
     <a href={`/blog/${post.meta.slug}`} className={styles.container}>
-        <ShowImage width={480} height={320} url={post.content_visuals[0]?.value?.url} className={styles.image} />
+      <ShowImage
+        width={480}
+        height={320}
+        url={post.content_visuals[0]?.value?.url}
+        className={styles.image}
+      />
       <div className={styles.content}>
         <h3>{post.title}</h3>
         <p>{post.intro.trim()}</p>
