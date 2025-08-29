a4daceb7ea178bc201bc6a49558eb9a4e115ce3d
Josh Martin <joshmartin@reprogrammed.software>
2025-08-29 18:31:05 -0500

    Enhances author info component
    Improves the author info component to enhance display and functionality.
    
    It updates the email link to open in a new tab for better user
    experience.
    Addresses potential security issues by sanitizing bio content using
    dangerouslySetInnerHTML.

--- app/blog/[slug]/AuthorInfo/AuthorInfo.tsx
+++ app/blog/[slug]/AuthorInfo/AuthorInfo.tsx
@@ -3,20 +3,27 @@ import styles from './styles.module.css'
 
 export function AuthorInfo({ bio, email, first_name, last_name, job_title, location, image, slug }: any) {
   return (
-    <div
-      className={styles.container}
-    >
+    <div className={styles.container}>
       <ShowImage url={image?.file} width={400} className={styles.authorImage} />
-      <div
-       className={styles.jobInfo} 
-      >
-        <h2>{first_name} {last_name}</h2>
+      <div className={styles.jobInfo}>
+        <h2>
+          {first_name} {last_name}
+        </h2>
         <h3>{job_title}</h3>
-        <h4>{email}</h4>
+        <h4>
+          <a
+            href={`mailto:${email}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "white" }}
+          >
+            {email}
+          </a>
+        </h4>
         <h4>{location}</h4>
-        {slug !== 'preview' &&
-          <div dangerouslySetInnerHTML={{__html: bio}} />
-        }
+        {slug !== "preview" && (
+          <div dangerouslySetInnerHTML={{ __html: bio }} />
+        )}
       </div>
     </div>
   );
