f16447bab6cc60d1fbbe1aeb9167b37a4935c93f
Josh Martin <joshmartin@reprogrammed.software>
2026-09-16 16:20:44 +0800

    Update footer text and externalize Next.js link

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -11,8 +11,8 @@ export default function Footer() {
         >
           <span style={{ backgroundColor: 'black' }}
           >
-          Handmade by <a href="https://github.com/cjoshmartin" target='_blank'>@cjoshmartin</a>,
-          with <a href="https://www.djangoproject.com/" target='_blank'>Django</a>, <a href='https://nextjs.org/'>Next.js</a> and{" "}
+          <s>Hand</s>made by a clanker and <a href="https://github.com/cjoshmartin" target='_blank'>@cjoshmartin</a>,
+          with <a href="https://www.djangoproject.com/" target='_blank'>Django</a>, <a href='https://nextjs.org/' target='_blank'>Next.js</a> and{" "}
           <a href="https://wagtail.org/" target='_blank'>Wagtail</a> (©{new Date().getFullYear()})
           </span>
         </span>
