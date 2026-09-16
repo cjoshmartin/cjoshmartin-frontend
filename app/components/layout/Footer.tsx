350b5c55b13941bfff14777322fec5aa1276e70f
Josh Martin <joshmartin@reprogrammed.software>
2026-09-16 16:48:47 +0800

    Update footer text to link 'clanker' to Claude AI

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
         >
           <span style={{ backgroundColor: 'black' }}
           >
-          <s>Hand</s>made by a clanker and <a href="https://github.com/cjoshmartin" target='_blank'>@cjoshmartin</a>,
+          <s>Hand</s>made by a <a href='https://claude.com/product/claude-code'>clanker</a> and <a href="https://github.com/cjoshmartin" target='_blank'>@cjoshmartin</a>,
           with <a href="https://www.djangoproject.com/" target='_blank'>Django</a>, <a href='https://nextjs.org/' target='_blank'>Next.js</a> and{" "}
           <a href="https://wagtail.org/" target='_blank'>Wagtail</a> (©{new Date().getFullYear()})
           </span>
